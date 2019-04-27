0a23bc4e02dd940577f9fe48c71a4f87cc4120fa
kharkovstiger <kharkovs.tiger@gmail.com>
2019-04-27 16:38:13 +0300

    refresh question

--- js/add.js
+++ js/add.js
@@ -1,7 +1,7 @@
 app.controller('addCtrl', ['$scope', '$http', function($scope, $http) {
 
     var baseURL='https://test-for-linor.herokuapp.com/api';
-    
+
     $scope.question={
         question: '',
         answers: [
@@ -16,7 +16,15 @@ app.controller('addCtrl', ['$scope', '$http', function($scope, $http) {
         console.log($scope.question);
         $http.post(baseURL+"/add", $scope.question).then(
             function (response) {
-                console.log(response.status+", "+response.data);
+                $scope.question={
+                    question: '',
+                    answers: [
+                        {answer: '', right: false},
+                        {answer: '', right: false},
+                        {answer: '', right: false},
+                        {answer: '', right: false}
+                    ]
+                };
             },
             function (response) {
                 console.log(response.status+", "+response.data);
